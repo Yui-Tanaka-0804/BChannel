dc8b0a162dfc8979e12b6fabfd447055a2ff89b5
Yui-Tanaka-0804 <yui.tanaka.0804@gmail.com>
2020-02-20 14:21:47 +0900

    [create]レス用のSeederを作成

---
+++ database/seeds/ResponseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Response;
+use Illuminate\Database\Seeder;
+
+class ResponseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Response::class, 10)->create();
+    }
+}
