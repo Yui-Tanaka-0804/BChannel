a137d96f901440ce56cf3a1a97bd85b88716243f
Yui-Tanaka-0804 <yui.tanaka.0804@gmail.com>
2020-07-10 11:07:37 +0900

    [add]管理者周りの機能を整備 - ユーザー登録画面を削除 - スレッドの削除機能を管理者メニューに移動 - authで弾かれたときのリンク先を'/index'に変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">管理者メニュー</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,13 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    @foreach ($data as $item)
+                    <form action="/{{ $item->id }}" method="POST">
+                        {{ csrf_field() }}
+                        @method('DELETE')
+                    <p>{{ $item->id . ". " }}<a href="{{ url()->current() . "/" . $item->id }}">{{ $item->name }}</a>{{"(".$item->responses_count().")"}} <input type="submit" value="削除" /></p>
+                    </form>
+                    @endforeach
                 </div>
             </div>
         </div>
