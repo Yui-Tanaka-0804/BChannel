0b832fa19db561dfa35b7852b8f1adaca669e5bd
Yui-Tanaka-0804 <yui.tanaka.0804@gmail.com>
2020-07-10 12:18:04 +0900

    [add]変更のないページはキャッシュを取得して表示するように変更

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,40 @@
 self.addEventListener('install', function (e) {
     console.log('ServiceWorker install')
+    caches.open(appKey)
+        .then(cache => {
+            cache.addAll([
+                '/login',
+                '/favicon.ico',
+                '/js/pwa.js',
+                '/manifest.json',
+                '/launcher-icon-4x.png',
+            ]);
+        })
   })
   
   self.addEventListener('activate', function (e) {
     console.log('ServiceWorker activate')
   })
   
-  self.addEventListener('fetch', function (event) {})
+  self.addEventListener('fetch', function (event) {
+    const url = new URL(request.url);
+
+    if(url.host === location.host) { // 自分のサイト内のURLの場合
+      // キャッシュ優先
+      const cachedResponse = await caches.match(request);
+      if(cachedResponse) {
+          return cachedResponse;
+      }
+      return fetch(request);
+    } else {
+      // ネットワーク優先
+      const cache = await caches.open(appKey);
+      try {
+          const response = await fetch(request);
+          await cache.put(request, response.clone());
+          return response;
+      } catch {
+          return await cache.match(request);
+      }
+    }
+  })
