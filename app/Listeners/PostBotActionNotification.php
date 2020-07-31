3a382efe7b474872747429bbe0c8bdd709cf94be
Yui-Tanaka-0804 <yui.tanaka.0804@gmail.com>
2020-07-31 11:18:11 +0900

    [change]botのコマンドをDB参照に変更

--- app/Listeners/PostBotActionNotification.php
+++ app/Listeners/PostBotActionNotification.php
@@ -32,43 +32,12 @@ class PostBotActionNotification implements ShouldQueue
         $command = $event->command;
 
         // 以下はbotの処理
-            if ($command == "ぬるぽ") {
-                $res = new \App\Response;
-                $res->thread_id = $thread_id;
-                $res->content = "ｶﾞｯ";
-                $res->save();
-            }
-            else if ($command == "ひよこ") {
-                $res = new \App\Response;
-                $res->thread_id = $thread_id;
-                $res->content = "
-　　　　　　　,-'\"ヽ　　　　
-　　　　　　/　　　i､　 　 　　　／￣￣　ヽ, 　　　　　＿/＼／＼/＼／|＿ 　
-　　　　　　{ ﾉ　　　\"' ゝ 　　 /　　　　　 　 ', 　　　　＼　　　　　　　　　　／
-　　　　　 ／　　 　 　　\"' ゝﾉ　{0}　 /¨`ヽ{0}　　　　 ＜　ニャーン！！　＞
-　　　 　 /　　　　　　　　　　　　　　ヽ._.ノ　　', 　　　／　 　　　　　　　　＼ 　　　　　　
-　　　 　i 　　　　　　　 　　　　　 　｀ー'′　 '.　　　　￣|／＼/＼/＼/￣　　　　　　　
-　　　　/ 　　　　　　　 　 　 　　　　　　　　 　}. 　　　　　　　　　
-　　　 i'　　　 /､　 　　　　　　 　　　 　　　　,i..　　　　　　　　　　
-　　　 い _／　 `-､.,,　　　　　､_　　　　　　　i　　　　　　　　　　
-　　 /'　/ 　 　 _/　 ＼`i　　　\"　　　/ﾞ　　 ./　　　　　　　　　　
-　 (,,／ 　 　 , '　 _,,-'\"　i　 ヾi__,,,...--t'\"　 ,|　　　　　　　　　　　
-　　　　　　 ,/　／　 　 　＼　 ヽ､　　　i　　|　　　　　　　　　　　
-　　　　　　 (､,,/　　　　　　 〉､ ､,}　　　 |　 .i　　　　　　　　　　
-　　　　　　　　　　　　　 　 `` `　　　　　! ､､＼　　　　　　　　　　
-　　　　　　　　　　　　　　 　　　　　　　　!､_n_,〉>
-
-　 　　　　　　　　　　　　　　　.,,......、
-　　　 ＿､　　　_　　　　　　　　 ヽ ｀'i　,‐..,　　　　　　＿＿＿,,,,,,,、
-　 '|ﾆ- ／ 　　!│　　　　　　　　,!　 ﾞ'\"　　l　　　　　l　　ﾞ　　　　ﾞl,　
-　　 ././　　　 .! ヽ　　　　　　　　!　 ,i--'\"゛　　　　　ﾞ'''\"'''/　 ,,r'''”
-　　 l .!　　　　 ! l ＼ 　　　　_,,,,,,,）　 |　　　　　　　　　,, 　｀ﾞ‐'゜
-　　 ! |　　　　/ |　ヽ`　　 ／..,,,,,_.　　 ｀''-､　　　　　,┘ﾞ,k　
-　　 ヽゝ-__-‐'ﾉ　　　　　 | .'(＿_./ 　.,、　 ｀'､.　　　|　 '{,,＿__,,,,,,,,､.〟
-　　　　─‐'''´　　　　　　　ヽ,、　　 _./ ｀'-､,,ﾉ .　　 'ｖ,_　 ￣｀　　: ,,,l
-　　　　　　　　　　　　　　　　 .￣´　　 　　　　　　　　　.ﾞ~ﾟ'冖''''\"'ﾞ”″
-                ";
-                $res->save();
-            }
+        if (\App\BotSpeak::where('command', $command)->exists()) {
+            $data = \App\BotSpeak::where('command', $command)->latest()->first();
+            $res = new \App\Response;
+            $res->thread_id = $thread_id;
+            $res->content = $data->content;
+            $res->save();
+        }
     }
 }
